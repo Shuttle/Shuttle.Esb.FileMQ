5877e7ca01a2737c17066fa87d7529bdc3fe74b2
eben-roux <me@ebenroux.co.za>
2014-10-29 09:44:43 +0200

    - initial commit

---
+++ Shuttle.ESB.FileMQ/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+#if NET35FULL
+[assembly: AssemblyTitle("Shuttle.ESB.FileMQ for .NET Framework 3.5")]
+#endif
+
+#if NET40FULL
+[assembly: AssemblyTitle("Shuttle.ESB.FileMQ for .NET Framework 4.0")]
+#endif
+
+#if NET45FULL
+[assembly: AssemblyTitle("Shuttle.ESB.FileMQ for .NET Framework 4.5")]
+#endif
+
+#if NET451FULL
+[assembly: AssemblyTitle("Shuttle.ESB.ModFileMQules for .NET Framework 4.5.1")]
+#endif
+
+[assembly: AssemblyVersion("3.3.5.0")]
+
