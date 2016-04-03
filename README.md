5360d6ffca182be891f07d21cbc71b4ff01c55e3
Eben <me@ebenroux.co.za>
2016-04-03 12:59:07 +0200

    - v3.8.9

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 Shuttle.Esb.FileMQ
 ==================
 
-File-based queue implementation for use with shuttle-esb.
+File-based queue implementation for use with Shuttle.Esb.
+
+This queue implementation is not intended to be used in a production environment other than for backing up / copying queue messages.
--- Shuttle.Esb.FileMQ/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.FileMQ/Properties/AssemblyInfo.cs
@@ -25,11 +25,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Esb.FileMQ for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("3.8.8.0")]
+[assembly: AssemblyVersion("3.8.9.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2016")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("3.8.8")]
+[assembly: AssemblyInformationalVersion("3.8.9")]
 [assembly: ComVisible(false)]
 
