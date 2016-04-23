451bafba8274a9275a1fdd5a9d24653e074de376
Eben <me@ebenroux.co.za>
2016-04-23 18:41:41 +0200

    - v3.8.10

--- Shuttle.Esb.FileMQ/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.FileMQ/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 #if NET40FULL
@@ -26,10 +26,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Esb.FileMQ for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("3.8.9.0")]
+[assembly: AssemblyVersion("3.8.10.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2016")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("3.8.9")]
+[assembly: AssemblyInformationalVersion("3.8.10")]
 [assembly: ComVisible(false)]
