2c54faaf512042adaa9d6b1a0a037ee23ed54453
eben-roux <me@ebenroux.co.za>
2018-07-04 20:12:56 +0200

    - v10.0.1

--- Shuttle.Esb.FileMQ/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.FileMQ/Properties/AssemblyInfo.cs
@@ -25,14 +25,18 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle(".NET Core 2.0")]
 #endif
 
+#if NETCOREAPP2_1
+[assembly: AssemblyTitle(".NET Core 2.1")]
+#endif
+
 #if NETSTANDARD2_0
 [assembly: AssemblyTitle(".NET Standard 2.0")]
 #endif
 
-[assembly: AssemblyVersion("10.0.0.0")]
+[assembly: AssemblyVersion("10.0.1.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2018")]
 [assembly: AssemblyProduct("Shuttle.Esb.FileMQ")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("10.0.0")]
+[assembly: AssemblyInformationalVersion("10.0.1")]
 [assembly: ComVisible(false)]
